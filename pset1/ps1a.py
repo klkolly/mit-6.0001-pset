d73233b6f8c9f2e8a8552cb463fa9baa399e424c
kong lingyu <975879575@qq.com>
2020-05-20 11:51:01 +0800

    Update ps1a.py

--- pset1/ps1a.py
+++ pset1/ps1a.py
@@ -1,21 +1,22 @@
-#import something
+#import something OR NOT
 
 #initial num
-current_savings = 0
-portion_down_payment = 0.25 
-#invest retrun  current_savings*r/12
-r = 0.04
-
-annual_salary = int(input("Enter your annual salary:  "))
-#dedicate a certain amount of your salary each month to saving forthe down payment.
-portion_saved =  float(input("Enter the percent of your salary to save, as a decimal:  "))
-total_cost = int(input("Enter the cost of your dream home:  "))
-
 def main():
+    current_savings = 0
+    portion_down_payment = 0.25
+    #invest retrun  current_savings*r/12
+    r = 0.04
+
+    annual_salary = int(input("Enter your annual salary:  "))
+    #dedicate a certain amount of your salary each month to saving forthe down payment.
+    portion_saved =  float(input("Enter the percent of your salary to save, as a decimal:  "))
+    total_cost = int(input("Enter the cost of your dream home:  "))
     month = 0
+    month_salary_portion=annual_salary*portion_saved/12
     while current_savings <= total_cost * portion_down_payment:
-        current_savings = annual_salary/12*portion_down_payment + current_savings/12 *r
+        current_savings += month_salary_portion + (current_savings*r/12 )
         month += 1
-       print(f"Number of months: {month}")
-    
+    print(f"Number of months: {month}")
+
 main()
+    
